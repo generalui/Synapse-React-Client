0d1329e832a86ddc089b0211d0f12f3817a991d9
Jon Ryser <241263+jonryser@users.noreply.github.com>
2020-11-24 13:11:38 -0800

    Updates

---
+++ precache-manifest.1cd2dcfc418f051188120d7427cb11bf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be981647ef9738983eab",
+    "url": "build/2.be981647.js"
+  },
+  {
+    "revision": "3eb3f9a71ca6779f4ed14b9b19b5eaf3",
+    "url": "build/2.be981647.js.LICENSE.txt"
+  },
+  {
+    "revision": "b788af5ed502bdb2a738",
+    "url": "build/3.b788af5e.js"
+  },
+  {
+    "revision": "6289d8a204ec847169a41c48a5c66bb0",
+    "url": "build/3.b788af5e.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bc29cb974ead5126ac",
+    "url": "build/4.65bc29cb.js"
+  },
+  {
+    "revision": "a4b12cbfd903692bb66f4c3fc5c2d7ed",
+    "url": "build/4.65bc29cb.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e03b146e0a917a4db39",
+    "url": "build/5.9e03b146.js"
+  },
+  {
+    "revision": "88c4e95ac146fe9845e9ebb5fb5097a4",
+    "url": "build/5.9e03b146.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c788390ddd0eb0d736",
+    "url": "build/bundle.38c78839.js"
+  },
+  {
+    "revision": "5ca0e1535b01fb8b730e",
+    "url": "build/main.5ca0e153.js"
+  },
+  {
+    "revision": "4574ffc96d0ae7d37d679ce9f12d4183",
+    "url": "index.html"
+  },
+  {
+    "revision": "5ca0e1535b01fb8b730e",
+    "url": "static/css/main.72a4b3a5.chunk.css"
+  },
+  {
+    "revision": "c6301611c7b7608ee8c05c4167b8509a",
+    "url": "static/media/brain.c6301611.svg"
+  },
+  {
+    "revision": "ed6d433ac9b20d9d3b317bd6395e8c3f",
+    "url": "static/media/circle.ed6d433a.svg"
+  },
+  {
+    "revision": "7e294e960c2fcacbdd85253b12fb64c4",
+    "url": "static/media/mouse.7e294e96.svg"
+  },
+  {
+    "revision": "7500217a7747733cdde4c98daef2c6a0",
+    "url": "static/media/resiliencead.7500217a.svg"
+  }
+]);
