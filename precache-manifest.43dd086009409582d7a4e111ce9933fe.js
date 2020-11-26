01e3024c84d3a0a635ca77ec19378da974df10ea
Batch Mosqueda <batch@genui.com>
2020-11-25 16:43:25 -0800

    Updates

---
+++ precache-manifest.43dd086009409582d7a4e111ce9933fe.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0a19a1fa813c8cd8cd",
+    "url": "build/2.1b0a19a1.js"
+  },
+  {
+    "revision": "c4c94906f37f19bd31c77f0aafcbb227",
+    "url": "build/2.1b0a19a1.js.LICENSE.txt"
+  },
+  {
+    "revision": "a925249922dba97aeb85",
+    "url": "build/3.a9252499.js"
+  },
+  {
+    "revision": "6f7c9da8b5b8d0cf2f528fe481d2f2b6",
+    "url": "build/3.a9252499.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ca68cd198b90e1a155",
+    "url": "build/4.e3ca68cd.js"
+  },
+  {
+    "revision": "dfd3cf7b69d063d4542fef7887a2d873",
+    "url": "build/4.e3ca68cd.js.LICENSE.txt"
+  },
+  {
+    "revision": "5194f83737818ef4bd3a",
+    "url": "build/5.5194f837.js"
+  },
+  {
+    "revision": "667e3befaed4c29ba9417b85f97d8daa",
+    "url": "build/5.5194f837.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac99276fc424eacece6",
+    "url": "build/bundle.5ac99276.js"
+  },
+  {
+    "revision": "bbce95f2df839e5f9a4d",
+    "url": "build/main.bbce95f2.js"
+  },
+  {
+    "revision": "b411a487b8ea8709f6ab1baf1dbd0043",
+    "url": "index.html"
+  },
+  {
+    "revision": "1b0a19a1fa813c8cd8cd",
+    "url": "static/css/2.e94d5e4c.chunk.css"
+  },
+  {
+    "revision": "bbce95f2df839e5f9a4d",
+    "url": "static/css/main.630f60ee.chunk.css"
+  },
+  {
+    "revision": "d8f80972135030bbfd82241a16aa46fd",
+    "url": "static/media/DNA_Two.d8f80972.svg"
+  },
+  {
+    "revision": "1104e430b37f10f989089a81f19c9de7",
+    "url": "static/media/Data2.1104e430.svg"
+  },
+  {
+    "revision": "77d5a35209d3d2ab122c528da455cfa6",
+    "url": "static/media/account-certified.77d5a352.svg"
+  },
+  {
+    "revision": "d8350d08ba80cf8b24d1bad037b5fda3",
+    "url": "static/media/account-registered.d8350d08.svg"
+  },
+  {
+    "revision": "db2e057242ae48d8822e67e5604b5abc",
+    "url": "static/media/account-validated.db2e0572.svg"
+  },
+  {
+    "revision": "c6301611c7b7608ee8c05c4167b8509a",
+    "url": "static/media/brain.c6301611.svg"
+  },
+  {
+    "revision": "1440a207455928cd9fa4a7244cc4f0b2",
+    "url": "static/media/calendar-clock.1440a207.svg"
+  },
+  {
+    "revision": "570e689419066429f57ca4dcc557086b",
+    "url": "static/media/chart2.570e6894.svg"
+  },
+  {
+    "revision": "ed6d433ac9b20d9d3b317bd6395e8c3f",
+    "url": "static/media/circle.ed6d433a.svg"
+  },
+  {
+    "revision": "2dcc2eacd52c0dd775bab52aa2affe2c",
+    "url": "static/media/file-dotted.2dcc2eac.svg"
+  },
+  {
+    "revision": "ff06cc8dd8e13db2c10c65fee930e104",
+    "url": "static/media/file.ff06cc8d.svg"
+  },
+  {
+    "revision": "e81d9a4f8c5ca4f8d84c837857a10c24",
+    "url": "static/media/icon_minus.e81d9a4f.svg"
+  },
+  {
+    "revision": "0e50c50103fd7290caffe43a432a444f",
+    "url": "static/media/icon_plus.0e50c501.svg"
+  },
+  {
+    "revision": "b1430018f94864e23a68584224e6826a",
+    "url": "static/media/icon_plus_square_filled.b1430018.svg"
+  },
+  {
+    "revision": "f3fa7393d64b08c7e18595bdf138baba",
+    "url": "static/media/icon_times.f3fa7393.svg"
+  },
+  {
+    "revision": "e64940c05806d6b24ea6c0c6b59a9725",
+    "url": "static/media/json-form-tool-no-submissions.e64940c0.svg"
+  },
+  {
+    "revision": "64d772c3e73d74ab9fca30aedeec9f99",
+    "url": "static/media/logo-white.64d772c3.svg"
+  },
+  {
+    "revision": "c5907af203b364d314935dbc7d00c12e",
+    "url": "static/media/logo.c5907af2.svg"
+  },
+  {
+    "revision": "4a97f235dff56c8702f244ad2ad9a5c1",
+    "url": "static/media/mouse.4a97f235.svg"
+  },
+  {
+    "revision": "7e294e960c2fcacbdd85253b12fb64c4",
+    "url": "static/media/mouse.7e294e96.svg"
+  },
+  {
+    "revision": "67d2a8a1d6b9d1c68bdba9379a228111",
+    "url": "static/media/organizations.67d2a8a1.svg"
+  },
+  {
+    "revision": "0d77b8e67fbb755e78a9c4cc614a4055",
+    "url": "static/media/person.0d77b8e6.svg"
+  },
+  {
+    "revision": "7500217a7747733cdde4c98daef2c6a0",
+    "url": "static/media/resiliencead.7500217a.svg"
+  },
+  {
+    "revision": "bc52e72be82449e98220e964df044c96",
+    "url": "static/media/study-active.bc52e72b.svg"
+  },
+  {
+    "revision": "b012f80b396b048881abcbfa63f2abd1",
+    "url": "static/media/study-complete.b012f80b.svg"
+  },
+  {
+    "revision": "f7a0ebbda4ba4a810f810d836f121093",
+    "url": "static/media/subscribe_plus.f7a0ebbd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8075e012b8220b443112b1ff906daded.js"
+  "precache-manifest.43dd086009409582d7a4e111ce9933fe.js"
 );
 
 self.addEventListener('message', (event) => {
